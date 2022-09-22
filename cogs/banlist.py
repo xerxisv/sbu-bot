988aecc01133f15951d4dc2bd707bce73850fd81
Xerxis <xerxis.webdev@gmail.com>
2022-09-22 14:44:39 +0300

    made banlist into a group command

--- cogs/banlist.py
+++ cogs/banlist.py
@@ -11,10 +11,34 @@ class BanList(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.Group
+    async def banlist(self, ctx: commands.Context):
+        return
+
+    @banlist.command()
+    async def help(self, ctx: commands.Context):
+        embed = discord.Embed(
+            title='Command help',
+            colour=0xc0c09e
+        )
+        embed.add_field(name=f'Add a user to banned-list',
+                        value='`+banlist add <IGN: text> [reason: text]`',
+                        inline=False)
+        embed.add_field(name=f'Remove a user from banned-list',
+                        value='`+banlist remove <IGN: text>`',
+                        inline=False)
+        embed.add_field(name='Check if a user if banned',
+                        value='`+banlist check <IGN: text>`')
+        embed.add_field(name='List all info related to the ban of the user',
+                        value='`+banlist info <IGN: text>`',
+                        inline=False)
+
+        await ctx.reply(embed=embed)
+
+    @banlist.command()
     @commands.has_role(MODERATOR_ROLE_ID)
-    async def banlist(self, ctx: commands.Context, banned_ign: str, *, reason: str = 'None'):
-        banned_id = BanList.extract_id(banned_ign)
+    async def add(self, ctx: commands.Context, banned_ign: str, *, reason: str = 'None'):
+        banned_id = extract_uuid(banned_ign)
 
         if banned_id is None:
             embed = discord.Embed(
@@ -38,6 +62,7 @@ class BanList(commands.Cog):
                 colour=0xFFFF00
             )
             await ctx.reply(embed=embed)
+            await db.close()
             return
 
         banned_member = BannedMember(banned_id, reason, ctx.author.id)  # Create banned member instance
@@ -73,17 +98,15 @@ class BanList(commands.Cog):
         await db.commit()
         await db.close()
 
-    @banlist.error
-    async def banlist_error(self, ctx, error):
-        if isinstance(error, commands.MissingRole):
-            await ctx.send("Insufficient Permissions")
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Incorrect format. Use `+banlist <IGN: text> [Reason: text]`")
+    @add.error
+    async def add_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Incorrect format. Use `+banlist add <IGN: text> [Reason: text]`")
 
-    @commands.command()
-    async def bancheck(self, ctx: commands.Context, banned_ign: str):
+    @banlist.command()
+    async def check(self, ctx: commands.Context, banned_ign: str):
 
-        banned_id = BanList.extract_id(banned_ign)
+        banned_id = extract_uuid(banned_ign)
 
         if banned_id is None:
             embed = discord.Embed(
@@ -104,7 +127,7 @@ class BanList(commands.Cog):
 
         if banned is None:
             embed = discord.Embed(
-                title='User not found',
+                title='Clear',
                 description='User is not present in our banned list',
                 colour=0x00FF00
             )
@@ -113,7 +136,7 @@ class BanList(commands.Cog):
             mod = await self.bot.get_or_fetch_user(banned['moderator'])
 
             embed = discord.Embed(
-                title='User found',
+                title='Not clear',
                 description='User is present in our banned list',
                 colour=0xFF0000
             )
@@ -122,20 +145,20 @@ class BanList(commands.Cog):
 
         await ctx.reply(embed=embed)
 
-    @bancheck.error
-    async def ban_check_error(self, ctx: commands.Context, exception: Exception):
+    @check.error
+    async def check_error(self, ctx: commands.Context, exception: Exception):
         if isinstance(exception, commands.MissingRequiredArgument):
             embed = discord.Embed(
                 title='Error',
-                description='Incorrect format. Use `+bancheck <IGN: text>`',
+                description='Incorrect format. Use `+banlist check <IGN: text>`',
                 colour=0xFF0000
             )
             await ctx.reply(embed=embed)
 
-    @commands.command()
+    @banlist.command()
     @commands.has_role(MODERATOR_ROLE_ID)
-    async def bandel(self, ctx: commands.Context, ign: str):
-        banned_uuid = BanList.extract_id(ign)
+    async def remove(self, ctx: commands.Context, ign: str):
+        banned_uuid = extract_uuid(ign)
 
         if banned_uuid is None:
             embed = discord.Embed(
@@ -146,13 +169,9 @@ class BanList(commands.Cog):
             await ctx.reply(embed=embed)
             return
 
-        db = await aiosqlite.connect(BannedMember.DB_PATH + BannedMember.DB_NAME + '.db')
-        cursor = await db.cursor()
+        banned = await fetch_user_from_db(banned_uuid)
 
-        await cursor.execute(BannedMember.select_row_with_id(banned_uuid))
-        res = await cursor.fetchone()
-
-        if res is None:
+        if banned is None:
             embed = discord.Embed(
                 title='Error',
                 description='User is not present in our database',
@@ -161,14 +180,20 @@ class BanList(commands.Cog):
             await ctx.reply(embed=embed)
             return
 
-        msg_id = BannedMember.dict_from_tuple(res)['message']
-        await ctx.guild.get_channel(BANNED_LIST_CHANNEL_ID).get_partial_message(msg_id).delete()
+        msg_id = banned['message']
+        try:
+            await ctx.guild.get_channel(BANNED_LIST_CHANNEL_ID).get_partial_message(msg_id).delete()
+        except discord.HTTPException:
+            pass
+
+        db = await aiosqlite.connect(BannedMember.DB_PATH + BannedMember.DB_NAME + '.db')
+        cursor = await db.cursor()
 
         await cursor.execute(BannedMember.delete_row_with_id(banned_uuid))
 
         embed = discord.Embed(
             title='Success',
-            description=f'User `{ign}` was removed from the banned database',
+            description=f'User `{ign}` was removed from <#{BANNED_LIST_CHANNEL_ID}>',
             colour=0x00FF00
         )
 
@@ -177,15 +202,88 @@ class BanList(commands.Cog):
 
         await ctx.reply(embed=embed)
 
-    @staticmethod
-    def extract_id(ign: str):
-        # Fetch user info
-        res = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{ign}')
+    @remove.error
+    async def remove_error(self, ctx: commands.Context, exception: Exception):
+        if isinstance(exception, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title='Error',
+                description='Incorrect format. Use `+banlist remove <IGN: text>`',
+                colour=0xFF0000
+            )
+            await ctx.reply(embed=embed)
 
-        if res.status_code != 200:  # Ensure that the request returned a user
-            return None
+    @banlist.command()
+    @commands.has_role(MODERATOR_ROLE_ID)
+    async def info(self, ctx: commands.Context, ign: str):
+        banned_uuid = extract_uuid(ign)
 
-        return res.json()['id']  # Return user's UUID
+        if banned_uuid is None:
+            embed = discord.Embed(
+                title='Error',
+                description='Invalid IGN',
+                colour=0xFF0000
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        banned = await fetch_user_from_db(banned_uuid)
+
+        if banned is None:
+            embed = discord.Embed(
+                title='Error',
+                description='User is not present in our database',
+                colour=0xFF0000
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        embed = discord.Embed(
+            title='Banned User Info',
+            colour=0xFFFF00
+        )
+
+        moderator = await self.bot.get_or_fetch_user(banned['moderator'])
+
+        embed.add_field(name='IGN', value=ign, inline=False)
+        embed.add_field(name='UUID', value=banned_uuid, inline=False)
+        embed.add_field(name='Banned by', value=f'{moderator.mention}', inline=False)
+        embed.add_field(name='Reason', value=banned['reason'], inline=False)
+        embed.add_field(name='Ban Date', value=f'<t:{banned["banned_at"]}>', inline=False)
+
+        await ctx.reply(embed=embed)
+
+    @info.error
+    async def info_error(self, ctx: commands.Context, exception: Exception):
+        if isinstance(exception, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title='Error',
+                description='Incorrect format. Use `+banlist check <IGN: text>`',
+                colour=0xFF0000
+            )
+            await ctx.reply(embed=embed)
+
+
+async def fetch_user_from_db(uuid: str):
+    db = await aiosqlite.connect(BannedMember.DB_PATH + BannedMember.DB_NAME + '.db')
+    cursor = await db.cursor()
+
+    await cursor.execute(BannedMember.select_row_with_id(uuid))
+    res = await cursor.fetchone()
+
+    if res is None:
+        return None
+
+    return BannedMember.dict_from_tuple(res)
+
+
+def extract_uuid(ign: str):
+    # Fetch user info
+    res = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{ign}')
+
+    if res.status_code != 200:  # Ensure that the request returned a user
+        return None
+
+    return res.json()['id']  # Return user's UUID
 
 
 def setup(bot):
