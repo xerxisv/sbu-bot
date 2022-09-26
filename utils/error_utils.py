cd7d38f02157f0e82219da0d519f4eec253d74b6
Xerxis <xerxis.webdev@gmail.com>
2022-09-26 12:17:21 +0300

    add message url to error logger

--- utils/error_utils.py
+++ utils/error_utils.py
@@ -3,16 +3,16 @@ import traceback
 
 from utils.constants import SBU_BOT_LOGS_CHANNEL_ID
 
-ERROR_MESSAGE = '**Exception raised during the execution of {}**.\n```{}```'
+ERROR_MESSAGE = '**Exception raised during the execution of {}**.\n```{}```*URL: <{}>*'
 
 
-def exception_to_string(command: str, exception: Exception):
+def exception_to_string(command: str, exception: Exception, jump_url: str = None):
     stack = traceback.extract_stack(limit=1)[:-3] + traceback.extract_tb(exception.__traceback__, limit=1)
     pretty = traceback.format_list(stack)
-    return ERROR_MESSAGE.format(command, ''.join(pretty) + '\n  {} {}'.format(exception.__class__, exception))
+    return ERROR_MESSAGE.format(command, ''.join(pretty) + '\n  {} {}'.format(exception.__class__, exception), jump_url)
 
 
 async def log_error(ctx: Context, exception: Exception):
     await ctx.guild\
         .get_channel(SBU_BOT_LOGS_CHANNEL_ID)\
-        .send(exception_to_string(ctx.command.name, exception))
+        .send(exception_to_string(ctx.command.name, exception, ctx.message.jump_url))
