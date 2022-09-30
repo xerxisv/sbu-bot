df507d930d079a3587e5754ed05f447d1acf88d0
Xerxis <xerxis.webdev@gmail.com>
2022-09-30 13:53:34 +0300

    fix exception on startup

--- utils/schemas/__init__.py
+++ utils/schemas/__init__.py
@@ -4,5 +4,4 @@ from .InactivePlayerSchema import InactivePlayer
 from .VerifiedMemberSchema import VerifiedMember
 from .BannedMemberSchema import BannedMember
 from .SuggestionSchema import Suggestion
-from .GuildTatsuSchema import GuildTatsu
 
