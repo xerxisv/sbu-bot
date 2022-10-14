737ff3ac87a79ee1efd63540f5dc62f1ac98beb0
Xerxis <xerxis.webdev@gmail.com>
2022-10-14 04:07:48 +0300

    change chat trigger handling

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import asyncio
 import os
-from random import choice
 
 import discord
 import discord.utils
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from handlers import chat_triggers
 from utils.constants import ADMIN_CHAT_CHANNEL_ID, BOT_OWNER_ROLE_ID, CARRY_SERVICE_REPS_CHANNEL_ID, \
     CRAFT_REPS_CHANNEL_ID, JR_MOD_ROLE_ID, SBU_BOT_LOGS_CHANNEL_ID, SBU_GOLD
+from utils.database import DBConnection
 from utils.error_utils import exception_to_string, log_error
 from utils.setup import run_setup
-from utils.database import DBConnection
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix="+", intents=intents, case_insensitive=True)
@@ -202,80 +202,10 @@ async def on_message(message: discord.Message):
     # Prevents the bot from going through the if statements unnecessarily when the message is a command or a bot reply
     if message.content.startswith('+') or message.author == bot.user:
         pass
-
-    elif message.content.upper() == "MEOW":
-        if message.author.id == 397389995113185293:
-            await message.reply("Meow")
-
-    elif message.content.upper() == "MEOWO":
-        if message.author.id == 397389995113185293:
-            await message.reply("UwU meow")
-
-    elif message.content.upper() == "FLOP":
-        if message.author.id == 615987518890049555:
-            array = ["<:turtleonfire:1021834121347084309>", "Fleee", "All hail King Flop",
-                     "https://i.imgur.com/pnruanZ.png"]
-            await message.reply(choice(array))
-
-    elif message.content.upper() == "PINGU":
-        if message.author.id == 381494697073573899:
-            array = [
-                "<a:poguin:933279319579561986>", "<a:pingupat:932962348908560417>", "UwU",
-                "https://tenor.com/view/noot-noot-apocalypse-gif-25788876"]
-            await message.reply(choice(array))
-
-    elif message.content.upper() == "NEO":
-        if message.author.id == 566329261535920175:
-            await message.reply("op")
-
-    elif message.content.upper() == "WINDOW":
-        if message.author.id == 797274543042986024:
-            await message.reply("https://tenor.com/view/monkey-gif-8660294")
-
-    elif message.content.upper() == "PLEB":
-        if message.author.id == 519985798393626634:
-            await message.reply("shitting on the bw gamers.")
-
-    elif message.content.upper() == "MEEP":
-        if message.author.id == 681475158975971329:
-            await message.reply("https://tenor.com/view/meap-phineas-and-ferb-phineas-and-ferb-meap-meep-gif-14038245")
-
-    elif message.content.upper() == "MEGA":
-        if message.author.id == 675106662302089247:
-            await message.reply("https://tenor.com/view/megalorian-tykhe-gif-24043314")
-
-    elif message.content.upper() == "HMM":
-        if message.author.id == 283326249735028736:
-            await message.reply("mhm")
-
-    elif message.content.upper() == "CHOMP":
-        if message.author.id == 241589674131456000:
-            await message.reply("https://tenor.com/view/cat-bite-funny-chomp-gif-16986241")
-
-    elif message.content.upper() in ["AGREED", 'I AGREE']:
-        if message.author.id == 309231901212672001:
-            await message.reply("https://tenor.com/view/metal-gear-rising-gif-25913914")
-
-    elif message.content.upper() == "CANNIBALISM":
-        if message.author.id in [606917358438580224, 241589674131456000]:
-            array = ["Pog!", "Noses!", "Toes!", "Fungus!", "https://i.imgur.com/IJ8URxl.jpg"]
-            await message.reply(choice(array))
-
-    elif message.content.upper() == "MUDKIP":
-        if message.author.id == 895488539775598603:
-            await message.reply("https://tenor.com/view/mudkip-spin-gif-24834722")
-
-    elif message.content == "RANDOM":
-        if message.author.id == 491654047741509633:
-            await message.reply("https://cdn.discordapp.com/emojis/967203616966459412")
-    elif message.content == "FIRE":
-        if message.author.id == 856492201529835521:
-            await message.reply("https://cdn.discordapp.com/emojis/943095611161460739.webp?size=128&quality=lossless")
-    elif message.content.upper() == "ZMAJ":
-        if message.author.id == 852647923875446816:
-            await message.reply("https://media.discordapp.net/attachments/1027212487008989284/1027212547591508070/cat-angry-cat.gif")
-
-    await bot.process_commands(message)
+    elif chat_triggers.is_trigger(message.content):
+        await chat_triggers.handle_trigger(message)
+    else:
+        await bot.process_commands(message)
 
 
 if __name__ == '__main__':
