0857eac4bb429bdad88118d2b6bf3ef357e3e0d3
Xerxis <xerxis.webdev@gmail.com>
2022-10-16 23:21:13 +0300

    fix old bot suggestion edit exception

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -131,72 +131,7 @@ class Suggestions(commands.Cog):
     @suggestion.command(name='approve', aliases=['yes', 'accept'])
     @commands.cooldown(1, 5)
     async def approve(self, ctx: commands.Context, suggestion_id: int, *, reason=None):
-        cursor: aiosqlite.Cursor = await self.db.cursor()
-
-        await cursor.execute(Suggestion.select_row_with_id(suggestion_id))
-        suggestion_tuple = await cursor.fetchone()
-
-        if suggestion_tuple is None:
-            await ctx.reply("Suggestion not found.")
-            return
-
-        suggestion = Suggestion.dict_from_tuple(suggestion_tuple)
-
-        suggestion_embed = discord.Embed(
-            title=f'Approved',
-            description=f"{suggestion['suggestion']}",
-            timestamp=datetime.datetime.utcnow(),
-            colour=0x00FF00
-        )
-
-        suggestion_author: discord.User = await self.bot.get_or_fetch_user(suggestion['author_id'])
-        suggestion_author = suggestion_author if suggestion_author is not None else suggestion["author_id"]
-
-        suggestion_embed.set_author(name=f'Suggested by {suggestion_author}')
-        suggestion_embed.add_field(name="Reason", value=f"{reason}", inline=False)
-        suggestion_embed.set_footer(text=f'Suggestion number {suggestion_id} | Approved by {ctx.author}')
-        suggestion_embed.set_thumbnail(
-            url=SBU_LOGO_URL)
-
-        message: discord.PartialMessage = self.bot \
-            .get_channel(SUGGESTIONS_CHANNEL_ID) \
-            .get_partial_message(suggestion['message_id'])
-        await message.edit(embed=suggestion_embed)
-
-        approved_embed = discord.Embed(
-            title=f'Approved',
-            description=f'Suggestion number {suggestion_id} approved successfully.',
-            timestamp=datetime.datetime.utcnow(),
-            colour=0x00FF00
-        )
-        try:
-            # Try DMing the user
-            await suggestion_author.send(embed=suggestion_embed)
-        except (discord.HTTPException, discord.Forbidden, AttributeError):
-            # DMing can throw because: API error, user having DMs closed/ bad intents or suggestion_author is an int
-            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
-
-        except Exception as exception:
-            # Any other error will be sent to the logs
-            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
-            await log_error(ctx, exception)
-
-        else:
-            # If no errors occurred send successful message
-            approved_embed \
-                .add_field(name="Direct Message", value=f"{suggestion_author} dmed successfully", inline=False)
-
-        finally:
-            # Send the embed regardless of errors
-            await ctx.send(embed=approved_embed, delete_after=10)
-
-            set_approved_tuple = Suggestion.set_approved(suggestion_id, True, ctx.author.id, reason)
-
-            await cursor.execute(*set_approved_tuple)
-            await cursor.close()
-            await self.db.commit()
-
-        await ctx.message.delete(delay=15)
+        await self.answer(ctx, suggestion_id, reason, True)
 
     @approve.error
     async def on_approve_error(self, ctx: commands.Context, exception):
@@ -209,68 +144,7 @@ class Suggestions(commands.Cog):
     @suggestion.command(name='deny', aliases=['no', 'decline'])
     @commands.cooldown(1, 5)
     async def deny(self, ctx, suggestion_id: int, *, reason=None):
-        cursor: aiosqlite.Cursor = await self.db.cursor()
-
-        await cursor.execute(Suggestion.select_row_with_id(suggestion_id))
-        suggestion_tuple = await cursor.fetchone()
-
-        if suggestion_tuple is None:
-            await ctx.reply("Suggestion not found.")
-            return
-
-        suggestion = Suggestion.dict_from_tuple(suggestion_tuple)
-
-        suggestion_embed = discord.Embed(
-            title=f'Denied',
-            description=f"{suggestion['suggestion']}",
-            timestamp=datetime.datetime.utcnow(),
-            colour=0xFF0000
-        )
-
-        suggestion_author: discord.User = await self.bot.get_or_fetch_user(suggestion['author_id'])
-        suggestion_author = suggestion_author if suggestion_author is not None else suggestion["author_id"]
-
-        suggestion_embed.set_author(name=f'Suggested by {suggestion_author}')
-        suggestion_embed.set_footer(text=f'Suggestion number {suggestion_id} | Denied by {ctx.author}')
-        suggestion_embed.add_field(name="Reason", value=f"{reason}", inline=False)
-        suggestion_embed.set_thumbnail(url=SBU_LOGO_URL)
-
-        message = self.bot.get_channel(SUGGESTIONS_CHANNEL_ID).get_partial_message(suggestion['message_id'])
-        await message.edit(embed=suggestion_embed)
-
-        approved_embed = discord.Embed(
-            title=f'Denied',
-            description=f'Suggestion number {suggestion_id} denied successfully.',
-            timestamp=datetime.datetime.utcnow(),
-            colour=0x0CE60C
-        )
-
-        try:
-            # Try DMing the user
-            await suggestion_author.send(embed=suggestion_embed)
-
-        except (discord.HTTPException, discord.Forbidden, AttributeError):
-            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
-
-        except Exception as exception:
-            # Any other error will be sent to the logs
-            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
-            await log_error(ctx, exception)
-
-        else:
-            # If no errors occurred send successful message
-            approved_embed.add_field(name="Direct Message", value=f"{suggestion_author} dmed successfully",
-                                     inline=False)
-        finally:
-            # Send the embed regardless of errors
-            await ctx.reply(embed=approved_embed, delete_after=10)
-
-            set_approved_tuple = Suggestion.set_approved(suggestion_id, False, ctx.author.id, reason)
-
-            await cursor.execute(*set_approved_tuple)
-            await cursor.close()
-            await self.db.commit()
-        await ctx.message.delete(delay=10)
+        await self.answer(ctx, suggestion_id, reason, False)
 
     @deny.error
     async def on_deny_error(self, ctx: commands.Context, exception):
@@ -596,6 +470,81 @@ class Suggestions(commands.Cog):
             return
 
         raise exception
+
+    async def answer(self, ctx: commands.Context, suggestion_id: int, reason: str, is_approved: bool):
+        cursor: aiosqlite.Cursor = await self.db.cursor()
+
+        await cursor.execute(Suggestion.select_row_with_id(suggestion_id))
+        suggestion_tuple = await cursor.fetchone()
+
+        if suggestion_tuple is None:
+            await ctx.reply("Suggestion not found.")
+            return
+
+        suggestion = Suggestion.dict_from_tuple(suggestion_tuple)
+
+        title = 'Approved' if is_approved else 'Denied'
+        color = 0x00FF00 if is_approved else 0xFF0000
+
+        suggestion_embed = discord.Embed(
+            title=title,
+            description=f"{suggestion['suggestion']}",
+            timestamp=datetime.datetime.utcnow(),
+            colour=color
+        )
+
+        suggestion_author: discord.User = await self.bot.get_or_fetch_user(suggestion['author_id'])
+        suggestion_author = suggestion_author if suggestion_author is not None else suggestion["author_id"]
+
+        suggestion_embed.set_author(name=f'Suggested by {suggestion_author}')
+        suggestion_embed.add_field(name="Reason", value=f"{reason}", inline=False)
+        suggestion_embed.set_footer(text=f'Suggestion number {suggestion_id} | {title} by {ctx.author}')
+        suggestion_embed.set_thumbnail(
+            url=SBU_LOGO_URL)
+
+        channel = self.bot.get_channel(SUGGESTIONS_CHANNEL_ID)
+        message: discord.Message = await channel.get_partial_message(suggestion['message_id']).fetch()
+
+        if message.author.id != self.bot.user.id:
+            await message.delete(reason='Old bot suggestion answer')
+            await channel.send(embed=suggestion_embed)
+        else:
+            await message.edit(embed=suggestion_embed)
+
+        approved_embed = discord.Embed(
+            title=title,
+            description=f'Suggestion number {suggestion_id} {title.lower()} successfully.',
+            timestamp=datetime.datetime.utcnow(),
+            colour=color
+        )
+        try:
+            # Try DMing the user
+            await suggestion_author.send(embed=suggestion_embed)
+        except (discord.HTTPException, discord.Forbidden, AttributeError):
+            # DMing can throw because: API error, user having DMs closed/ bad intents or suggestion_author is an int
+            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
+
+        except Exception as exception:
+            # Any other error will be sent to the logs
+            approved_embed.add_field(name="Direct Message", value=f"User could not be dmed", inline=False)
+            await log_error(ctx, exception)
+
+        else:
+            # If no errors occurred send successful message
+            approved_embed \
+                .add_field(name="Direct Message", value=f"{suggestion_author} dmed successfully", inline=False)
+
+        finally:
+            # Send the embed regardless of errors
+            await ctx.send(embed=approved_embed, delete_after=10)
+
+            set_approved_tuple = Suggestion.set_approved(suggestion_id, is_approved, ctx.author.id, reason)
+
+            await cursor.execute(*set_approved_tuple)
+            await cursor.close()
+            await self.db.commit()
+
+        await ctx.message.delete(delay=10)
 
 
 def setup(bot):
